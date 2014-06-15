3d652bc6a6526c80288d0e4546fc179fdb2af465
M.S. List <simlist@gmail.com>
2014-06-14 23:03:39 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name=luachcal,
+      version='0.1.0dev',
+      author='MS List',
+      author_email='simlist@gmail.com',
+      packages ='luachcal',
+      url='https://github.com/simlist/luachcal',
+      license='',  # need to add
+      description =("""package for working with Hebrew dates and
+      Gregorian-Hebrew conversion"""),
+      long_description=''  # add external file as in open('filename').read()
+       )
