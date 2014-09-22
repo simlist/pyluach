d755d7683e42f6f8cd90792ccb7f7efc549e5b60
M.S. List <simlist@gmail.com>
2014-09-22 10:04:39 -0400

    Added comma between combined parshios.

--- luachcal/parshios.py
+++ luachcal/parshios.py
@@ -62,7 +62,7 @@ def _gentable(year, israel=False):
                (parsha == 50 and HebrewDate(year+1, 7, 1).weekday() > 4)
                ):  #  If any of that then it's a double parsha.
                 key = shabbos.tuple()
-                table[key] = ' '.join([
+                table[key] = ', '.join([
                                       table[key],
                                       PARSHIOS[parshalist.popleft()]
                                       ])
