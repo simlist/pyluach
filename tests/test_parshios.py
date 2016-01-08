cd360c66e85f6ba5c85404f81a3d87ae6bf70efc
Meir S. List <simlist@gmail.com>
2016-01-07 19:53:33 -0500

    Removed failing empty parsha test.

--- tests/test_parshios.py
+++ tests/test_parshios.py
@@ -5,9 +5,7 @@ from pyluach import parshios
 KNOWN_VALUES = {
     (1, 7, 2016): "Va'era"
     }
-class Test_test1(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+
 
 if __name__ == '__main__':
     unittest.main()
