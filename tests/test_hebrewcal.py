c9ad8a3fd4b486f00c6caa2d38c2045374705370
Meir S. List <simlist@gmail.com>
2016-01-27 13:14:50 -0500

    Test work.

--- tests/test_hebrewcal.py
+++ tests/test_hebrewcal.py
@@ -2,9 +2,13 @@ import unittest
 
 from pyluach import dates, hebrewcal
 
-class Test_test_hebrewcal(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+class TestMonth(unittest.TestCase):
+    def test_subtract_month(self):
+        month1 = hebrewcal.Month(5775, 10)
+        month2 = hebrewcal.Month(5776, 10)
+        month3 = hebrewcal.Month(5777, 10)
+        self.assertEqual(month1 - month2, 12)
+        self.assertEqual(month3 - month1, 25)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_parshios.py
+++ tests/test_parshios.py
@@ -1,11 +1,18 @@
 import unittest
 
-from pyluach import parshios
+from pyluach import parshios, dates
 
 KNOWN_VALUES = {
-    (1, 7, 2016): "Va'era"
+    (2016, 1, 7): 13,
+    (2017, 3, 21): [21, 22]
     }
 
+class TestGetParsha(unittest.TestCase):
+    
+    def test_getparsha(self):
+        for key in KNOWN_VALUES:
+            self.assertEqual(parshios.getparsha(dates.GregorianDate(*key)),
+                             KNOWN_VALUES[key])
 
 if __name__ == '__main__':
     unittest.main()
