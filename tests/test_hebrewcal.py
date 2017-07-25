81ca561574328f5c0fcedc8f3a11eb196ef408cc
Meir S. List <simlist@gmail.com>
2017-07-25 10:59:14 -0500

    Added tests.

--- tests/test_hebrewcal.py
+++ tests/test_hebrewcal.py
@@ -1,14 +1,38 @@
-import unittest
+import pytest
 
 from pyluach import dates, hebrewcal
 
-class TestMonth(unittest.TestCase):
+class TestYear(object):
+
+    def test_equalyear(self):
+        year1 = hebrewcal.Year(5777)
+        year2 = hebrewcal.Year(5777)
+        assert year1 == year2
+
+class TestMonth(object):
+
+    def test_equalmonth(self):
+        month1 = hebrewcal.Month(5777, 12)
+        month2 = hebrewcal.Month(5777, 12)
+        assert month1 == month2
+
+    def test_addinttomonth(self):
+        month = hebrewcal.Month(5777, 12)
+        assert month + 0 == month
+        assert month + 1 == hebrewcal.Month(5777, 1)
+        assert month + 6 == hebrewcal.Month(5777, 6)
+        assert month + 7 == hebrewcal.Month(5778, 7)
+        assert month + 35 == hebrewcal.Month(5780, 10)
+
     def test_subtract_month(self):
         month1 = hebrewcal.Month(5775, 10)
         month2 = hebrewcal.Month(5776, 10)
         month3 = hebrewcal.Month(5777, 10)
-        self.assertEqual(month1 - month2, 12)
-        self.assertEqual(month3 - month1, 25)
+        assert month1 - month2 == 12
+        assert month3 - month1 == 25
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_subtractintfrommonth(self):
+        month = hebrewcal.Month(5778, 9)
+        assert month - 2 == hebrewcal.Month(5778, 7)
+        assert month - 3 == hebrewcal.Month(5777, 6)
+        assert month - 30 == hebrewcal.Month(5775, 4)
