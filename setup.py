c42444b06e82bc3259e871c92a6e60977eec4823
simlist <simlist@gmail.com>
2020-11-08 10:09:02 -0500

    Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import io
 
 setup(name='pyluach',
-      version='1.2.0',
+      version='1.2.1',
       author='MS List',
       author_email='simlist@gmail.com',
       packages=['pyluach', ],
