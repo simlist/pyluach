511e214f5694112ef19d3d33fe95526753b45f51
MS List <simlist@gmail.com>
2022-05-20 10:48:56 -0400

    Wrote tests for bug where comparing months.

--- tests/test_hebrewcal.py
+++ tests/test_hebrewcal.py
@@ -242,22 +242,26 @@ class TestCompareMonth:
         assert (months[1] > months[2]) is False
         assert months[3] > months[1]
         assert (months[2] > months[3]) is False
+        assert months[1] > Month(6780, 12)
 
     def test_month_ge(self, months):
         assert copy(months[1]) >= months[1]
         assert months[2] >= months[1]
         assert (months[2] >= months[3]) is False
+        assert months[1] >= Month(6780, 12)
 
     def test_month_lt(self, months):
         assert (copy(months[2]) < months[2]) is False
         assert months[1] < months[2]
         assert months[2] < months[3]
         assert (months[3] < months[1]) is False
+        assert Month(6780, 12) < months[1]
 
     def test_month_le(self, months):
         assert copy(months[2]) <= months[2]
         assert months[1] <= months[2]
         assert (months[3] <= months[2]) is False
+        assert Month(6780, 12) <= months[1]
 
     def rest_month_ne(self, months):
         assert months[2] != months[1]
