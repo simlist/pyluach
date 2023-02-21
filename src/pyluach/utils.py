3d3ac362571dc6e6b75c0d72c17427635cf90b81
MS List <simlist@gmail.com>
2023-02-21 15:43:46 -0500

    Added Days enum to utils.

--- src/pyluach/utils.py
+++ src/pyluach/utils.py
@@ -3,6 +3,30 @@
 They are to be used internally.
 """
 from functools import lru_cache
+from enum import Enum
+
+
+class Days(Enum):
+    ROSH_HASHANA = 'Rosh Hashana'
+    YOM_KIPPUR = 'Yom Kippur'
+    SUCCOS = 'Succos'
+    SHMINI_ATZERES = 'Shmini Atzeres'
+    SIMCHAS_TORAH = 'Simchas Torah'
+    CHANUKA = 'Chanuka'
+    TU_BSHVAT = "Tu B'shvat"
+    PURIM_KATAN = 'Purim Katan'
+    PURIM = 'Purim'
+    SHUSHAN_PURIM = 'Shushan Purim'
+    PESACH = 'Pesach'
+    PESACH_SHENI = 'Pesach Sheni'
+    LAG_BAOMER = "Lag Ba'omer"
+    SHAVUOS = 'Shavuos'
+    TU_BAV = "Tu B'av"
+    TZOM_GEDALIA = 'Tzom Gedalia'
+    TENTH_OF_TEVES = '10 of Teves'
+    TAANIS_ESTHER = 'Taanis Esther'
+    SEVENTEENTH_OF_TAMUZ = '17 of Tamuz'
+    NINTH_OF_AV = '9 of Av'
 
 
 MONTH_NAMES = [
