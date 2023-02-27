08866d3c993d8c2441e4f717bba69d8d60c9ce1e
MS List <simlist@gmail.com>
2023-02-27 11:37:13 -0500

    Incremented version.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ copyright = '2016, MS List'
 author = 'MS List'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.2.0'
 
 
 # -- General configuration ---------------------------------------------------
