bca7a5579306c773e9c5d74614249c100607c1d5
MS List <simlist@gmail.com>
2026-01-27 20:37:57 -0500

    Began languages module.

---
+++ src/pyluach/languages.py
@@ -0,0 +1,112 @@
+from pyluach import names
+
+"""
+class ParshiosNames(Enum):
+    BEREISHIS = 'בראשית'
+    NOACH = 'נח'
+    LECH_LECHA = 'לך לך'
+    VAYEIRA = 'וירא'
+    CHAYEI_SARAH = 'חיי שרה'
+    TOLDOS = 'תולדות'
+    VAYEITZEI = 'ויצא'
+    VAYISHLACH = 'וישלח'
+    VAYEISHEV = 'וישב'
+    MIKEITZ = 'מקץ'
+    VAYIGASH = 'ויגש'
+    VAYECHI = 'ויחי'
+    SHEMOS = 'שמות'
+    VAEIRA = 'וארא'
+    BO = 'בא'
+    BESHALACH = 'בשלח'
+    YISRO = 'יתרו'
+    MISHPATIM = 'משפטים'
+    TERUMA = 'תרומה'
+    TETZAVEH = 'תצוה'
+    KI_SISA = 'כי תשא'
+    VAYAKHEL = 'ויקהל'
+    PEKUDEI = 'פקודי'
+    VAYIKRA = 'ויקרא'
+    TZAV = 'צו'
+    SHEMINI = 'שמיני'
+    TAZRIA = 'תזריע'
+    METZORA = 'מצורע'
+    ACHAREI_MOS = 'אחרי מות'
+    KEDOSHIM = 'קדושים'
+    EMOR = 'אמור'
+    BEHAR = 'בהר'
+    BECHUKOSAI = 'בחוקותי'
+    BAMIDBAR = 'במדבר'
+    NASSO = 'נשא'
+    BEHAALOSCHA = 'בהעלותך'
+    SHELACH = 'שלח'
+    KORACH = 'קרח'
+    CHUKAS = 'חקת'
+    BALAK = 'בלק'
+    PINCHAS = 'פינחס'
+    MATTOS = 'מטות',
+    MASEI = 'מסעי'
+    DEVARIM = 'דברים'
+    VAESCHANAN = 'ואתחנן'
+    EIKEV = 'עקב'
+    REEH = 'ראה'
+    SHOFTIM = 'שופטים'
+    KI_SEITZEI = 'כי תצא'
+    KI_SAVO = 'כי תבא'
+    NETZAVIM = 'נצבים'
+    VAYEILECH = 'וילך'
+    HAAZINU = 'האזינו'
+    VEZOS_HABERACHA = 'וזאת הברכה'
+
+
+class FourParshiosNames(Enum):
+    ZACHOR = 'זכור'
+    SHEKALIM = 'שקלים'
+    PARAH = 'פרה'
+    HACHODESH = 'החודש'
+
+
+class DaysNames(Enum):
+    ROSH_HASHANA = 'ראש השנה'
+    YOM_KIPPUR = 'יום כיפור'
+    SUCCOS = 'סוכות'
+    SHMINI_ATZERES = 'שמיני עצרת'
+    SIMCHAS_TORAH = 'שמחת תורה'
+    CHANUKA = 'חנוכה'
+    TU_BSHVAT = 'ט״ו בשבט'
+    PURIM_KATAN = 'פורים קטן'
+    PURIM = 'פורים'
+    SHUSHAN_PURIM = 'שושן פורים'
+    PESACH = 'פסח'
+    PESACH_SHENI = 'פסח שני'
+    LAG_BAOMER = 'ל״ג בעומר'
+    SHAVUOS = 'שבועות'
+    TU_BAV = 'ט״ו באב'
+    TZOM_GEDALIA = 'צום גדליה'
+    TENTH_OF_TEVES = 'י׳ בטבת'
+    TAANIS_ESTHER = 'תענית אסתר'
+    SEVENTEENTH_OF_TAMUZ = 'י״ז בתמוז'
+    NINTH_OF_AV = 'ט׳ באב'
+"""
+
+
+english = {
+    names.MonthNames.NISSAN: 'Nissan',
+    names.MonthNames.IYAR: 'Iyar',
+    names.MonthNames.SIVAN: 'Sivan',
+    names.MonthNames.TAMMUZ: 'Tammuz',
+    names.MonthNames.AV: 'Av',
+    names.MonthNames.ELUL: 'Elul',
+    names.MonthNames.TISHREI: 'Tishrei',
+    names.MonthNames.CHESHVAN: 'Cheshvan',
+    names.MonthNames.KISLEV: 'Kislev',
+    names.MonthNames.TEVES: 'Teves',
+    names.MonthNames.SHEVAT: 'Shevat',
+    names.MonthNames.ADAR: 'Adar',
+    names.MonthNames.ADAR1: 'Adar 1',
+    names.MonthNames.ADAR2: 'Adar 2',
+    names.ParshiosNames.BEREISHIS: 'Bereishis',
+    names.ParshiosNames.NOACH: 'Noach',
+    names.ParshiosNames.LECH_LECHA: 'Lech Lecha',
+    names.ParshiosNames.VAYEIRA: 'Vayeira',
+    names.ParshiosNames.CHAYEI_SARAH: 'Chayei_Sarah'
+}
