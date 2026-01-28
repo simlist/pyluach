0b61d21faf0e3794954afa74befab4e6881c72d2
MS List <simlist@gmail.com>
2026-01-28 16:46:45 -0500

    Added to english_ashkenazik dict.

--- src/pyluach/languages.py
+++ src/pyluach/languages.py
@@ -1,71 +1,7 @@
-from pyluach import names
+from pyluach.names import Parshios, Months, FourParshios, Days
 
 """
-class ParshiosNames(Enum):
-    BEREISHIS = 'בראשית'
-    NOACH = 'נח'
-    LECH_LECHA = 'לך לך'
-    VAYEIRA = 'וירא'
-    CHAYEI_SARAH = 'חיי שרה'
-    TOLDOS = 'תולדות'
-    VAYEITZEI = 'ויצא'
-    VAYISHLACH = 'וישלח'
-    VAYEISHEV = 'וישב'
-    MIKEITZ = 'מקץ'
-    VAYIGASH = 'ויגש'
-    VAYECHI = 'ויחי'
-    SHEMOS = 'שמות'
-    VAEIRA = 'וארא'
-    BO = 'בא'
-    BESHALACH = 'בשלח'
-    YISRO = 'יתרו'
-    MISHPATIM = 'משפטים'
-    TERUMA = 'תרומה'
-    TETZAVEH = 'תצוה'
-    KI_SISA = 'כי תשא'
-    VAYAKHEL = 'ויקהל'
-    PEKUDEI = 'פקודי'
-    VAYIKRA = 'ויקרא'
-    TZAV = 'צו'
-    SHEMINI = 'שמיני'
-    TAZRIA = 'תזריע'
-    METZORA = 'מצורע'
-    ACHAREI_MOS = 'אחרי מות'
-    KEDOSHIM = 'קדושים'
-    EMOR = 'אמור'
-    BEHAR = 'בהר'
-    BECHUKOSAI = 'בחוקותי'
-    BAMIDBAR = 'במדבר'
-    NASSO = 'נשא'
-    BEHAALOSCHA = 'בהעלותך'
-    SHELACH = 'שלח'
-    KORACH = 'קרח'
-    CHUKAS = 'חקת'
-    BALAK = 'בלק'
-    PINCHAS = 'פינחס'
-    MATTOS = 'מטות',
-    MASEI = 'מסעי'
-    DEVARIM = 'דברים'
-    VAESCHANAN = 'ואתחנן'
-    EIKEV = 'עקב'
-    REEH = 'ראה'
-    SHOFTIM = 'שופטים'
-    KI_SEITZEI = 'כי תצא'
-    KI_SAVO = 'כי תבא'
-    NETZAVIM = 'נצבים'
-    VAYEILECH = 'וילך'
-    HAAZINU = 'האזינו'
-    VEZOS_HABERACHA = 'וזאת הברכה'
-
-
-class FourParshiosNames(Enum):
-    ZACHOR = 'זכור'
-    SHEKALIM = 'שקלים'
-    PARAH = 'פרה'
-    HACHODESH = 'החודש'
-
-
-class DaysNames(Enum):
+class DaysEnum):
     ROSH_HASHANA = 'ראש השנה'
     YOM_KIPPUR = 'יום כיפור'
     SUCCOS = 'סוכות'
@@ -89,24 +25,97 @@ class DaysNames(Enum):
 """
 
 
-english = {
-    names.MonthNames.NISSAN: 'Nissan',
-    names.MonthNames.IYAR: 'Iyar',
-    names.MonthNames.SIVAN: 'Sivan',
-    names.MonthNames.TAMMUZ: 'Tammuz',
-    names.MonthNames.AV: 'Av',
-    names.MonthNames.ELUL: 'Elul',
-    names.MonthNames.TISHREI: 'Tishrei',
-    names.MonthNames.CHESHVAN: 'Cheshvan',
-    names.MonthNames.KISLEV: 'Kislev',
-    names.MonthNames.TEVES: 'Teves',
-    names.MonthNames.SHEVAT: 'Shevat',
-    names.MonthNames.ADAR: 'Adar',
-    names.MonthNames.ADAR1: 'Adar 1',
-    names.MonthNames.ADAR2: 'Adar 2',
-    names.ParshiosNames.BEREISHIS: 'Bereishis',
-    names.ParshiosNames.NOACH: 'Noach',
-    names.ParshiosNames.LECH_LECHA: 'Lech Lecha',
-    names.ParshiosNames.VAYEIRA: 'Vayeira',
-    names.ParshiosNames.CHAYEI_SARAH: 'Chayei_Sarah'
+english_ashkenazic = {
+    Months.NISSAN: 'Nissan',
+    Months.IYAR: 'Iyar',
+    Months.SIVAN: 'Sivan',
+    Months.TAMMUZ: 'Tammuz',
+    Months.AV: 'Av',
+    Months.ELUL: 'Elul',
+    Months.TISHREI: 'Tishrei',
+    Months.CHESHVAN: 'Cheshvan',
+    Months.KISLEV: 'Kislev',
+    Months.TEVES: 'Teves',
+    Months.SHEVAT: 'Shevat',
+    Months.ADAR: 'Adar',
+    Months.ADAR1: 'Adar 1',
+    Months.ADAR2: 'Adar 2',
+    Parshios.BEREISHIS: 'Bereishis',
+    Parshios.NOACH: 'Noach',
+    Parshios.LECH_LECHA: 'Lech Lecha',
+    Parshios.VAYEIRA: 'Vayeira',
+    Parshios.CHAYEI_SARAH: 'Chayei Sarah',
+    Parshios.TOLDOS: 'Toldos',
+    Parshios.VAYEITZEI: 'Vayeitzei',
+    Parshios.VAYISHLACH: 'Vayishlach',
+    Parshios.VAYEISHEV: 'Vayeishev',
+    Parshios.MIKEITZ: 'Mikeitz',
+    Parshios.VAYIGASH: 'Vayigash',
+    Parshios.VAYECHI: 'Vayechi',
+    Parshios.SHEMOS: 'Shemos',
+    Parshios.VAEIRA: "Va'eira",
+    Parshios.BO: 'Bo',
+    Parshios.BESHALACH: 'Beshalach',
+    Parshios.YISRO: 'Yisro',
+    Parshios.MISHPATIM: 'Mishpatim',
+    Parshios.TERUMAH: 'Terumah',
+    Parshios.TETZAVEH: 'Tetzaveh',
+    Parshios.KI_SISA: 'Ki Sisa',
+    Parshios.VAYAKHEL: 'Vayakhel',
+    Parshios.PEKUDEI: 'Pekudei',
+    Parshios.VAYIKRA: 'Vayikra',
+    Parshios.TZAV: 'Tzav',
+    Parshios.SHEMINI: 'Shemini',
+    Parshios.TAZRIA: 'Tazria',
+    Parshios.METZORA: 'Metzora',
+    Parshios.ACHAREI_MOS: 'Acharei Mos',
+    Parshios.KEDOSHIM: 'Kedoshim',
+    Parshios.EMOR: 'Emor',
+    Parshios.BEHAR: 'Behar',
+    Parshios.BECHUKOSAI: 'Bechukosai',
+    Parshios.BAMIDBAR: 'Bamidbar',
+    Parshios.NASSO: 'Nasso',
+    Parshios.BEHAALOSCHA: "Beha'aloscha",
+    Parshios.SHELACH: 'Shelach',
+    Parshios.KORACH: 'Korach',
+    Parshios.CHUKAS: 'Chukas',
+    Parshios.BALAK: 'Balak',
+    Parshios.PINCHAS: 'Pinchas',
+    Parshios.MATTOS: 'Mattos',
+    Parshios.MASEI: 'Masei',
+    Parshios.DEVARIM: 'Devarim',
+    Parshios.VAESCHANAN: "Va'eschanan",
+    Parshios.EIKEV: 'Eikev',
+    Parshios.REEH: "Re'eh",
+    Parshios.SHOFTIM: 'Shoftim',
+    Parshios.KI_SEITZEI: 'Ki Seitzei',
+    Parshios.KI_SAVO: 'Ki Savo',
+    Parshios.NITZAVIM: 'Nitzavim',
+    Parshios.VAYEILECH: 'Vayeilech',
+    Parshios.HAAZINU: 'Haazinu',
+    Parshios.VEZOS_HABERACHA: 'Vezos Haberacha',
+    FourParshios.SHEKALIM: 'Shekalim',
+    FourParshios.ZACHOR: 'Zachor',
+    FourParshios.PARAH: 'Parah',
+    FourParshios.HACHODESH: 'Hachodesh',
+    Days.ROSH_HASHANA: 'Rosh Hashana',
+    Days.YOM_KIPPUR: 'Yom Kippur',
+    Days.SUCCOS: 'Succos',
+    Days.SHMINI_ATZERES: 'Shmini Atzeres',
+    Days.SIMCHAS_TORAH: 'Simchas Torah',
+    Days.CHANUKA: 'Chanuka',
+    Days.TU_BSHVAT: "Tu B'shvat",
+    Days.PURIM_KATAN: 'Purim Katan',
+    Days.PURIM: 'Purim',
+    Days.SHUSHAN_PURIM: 'Shushan Purim',
+    Days.PESACH: 'Pesach',
+    Days.PESACH_SHENI: 'Pesach Sheni',
+    Days.LAG_BAOMER: "Lag Ba'omer",
+    Days.SHAVUOS: 'Shavuos',
+    Days.TU_BAV: "Tu B'av",
+    Days.TZOM_GEDALIA: 'Tzom Gedalia',
+    Days.TENTH_OF_TEVES: '10 of Teves',
+    Days.TAANIS_ESTHER: 'Taanis Esther',
+    Days.SEVENTEENTH_OF_TAMMUZ: '17 of Tammuz',
+    Days.NINTH_OF_AV: '9 of Av'
 }
