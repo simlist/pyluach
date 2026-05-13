8622d74a45f59872ae37eaf806b7adabdbed1576
MS List <simlist@gmail.com>
2026-05-13 16:59:14 -0400

    Incremented version number.

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,10 @@ import sphinx_rtd_theme
 
 project = 'pyluach'
 copyright = '2016, MS List'
-author = 'MS List'
+author = 'Simcha List'
 
 # The full version, including alpha/beta/rc tags
-release = '2.3.0'
+release = '2.4.0'
 
 
 # -- General configuration ---------------------------------------------------
